7fd487d93b350733ec9f25fa7ddfa626a8535c31
Prio <miprio101@gmail.com>
2018-08-29 00:53:01 +0600

    Update pass

--- config/dev.exs
+++ config/dev.exs
@@ -4,6 +4,6 @@ config :todolist, TodoList.Repo, [
   adapter: Ecto.Adapters.Postgres,
   database: "todolist_#{Mix.env}",
   username: "postgres",
-  password: "postgres",
+  password: "",
   hostname: "localhost",
 ]
